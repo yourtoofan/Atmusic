70cbf81c395eb9abdeb47581a1f96cdb3bea02cf
Legend mickey <toofanpapa424@gmail.com>
2024-11-13 15:14:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/yourtoofan/Zoyu1",
+    "https://github.com/yourtoofan/Atmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
